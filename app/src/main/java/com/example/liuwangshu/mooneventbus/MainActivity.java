15888ae4d5081e70a484d106963aa4f5a4392819
Liu wangshu <525010411@qq.com>
2017-06-09 11:57:45 +0800

    重复订阅事件处理

--- app/src/main/java/com/example/liuwangshu/mooneventbus/MainActivity.java
+++ app/src/main/java/com/example/liuwangshu/mooneventbus/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
@@ -34,7 +36,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //注册事件
-                EventBus.getDefault().register(MainActivity.this);
+                if(!EventBus.getDefault().isRegistered(MainActivity.this)) {
+                    EventBus.getDefault().register(MainActivity.this);
+                }else{
+                    Toast.makeText(MainActivity.this,"请勿重复注册事件",Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
